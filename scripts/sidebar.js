6aedd220e005a96e5027ad7b881002e6bfb42128
Gaxown <th3deziner@gmail.com>
2024-10-25 17:07:51 +0200

    VCV-31 w3c validation & cleaning code js and css

--- scripts/sidebar.js
+++ scripts/sidebar.js
@@ -1,13 +1,41 @@
 const menuButton = document.getElementById("menu-button");
 const sidebar = document.getElementById("sidebar");
 const overlay = document.getElementById("overlay");
+const linkElements = document.querySelectorAll(".link");
 
+// Function to toggle sidebar and overlay
+const toggleSidebar = (isOpen) => {
+  sidebar.classList.toggle("open", isOpen);
+  overlay.classList.toggle("open", isOpen);
+};
+
+// Event listeners
 menuButton.addEventListener("click", () => {
-  sidebar.classList.add("open");
-  overlay.classList.add("open");
+  const isOpen = sidebar.classList.contains("open");
+  toggleSidebar(!isOpen); // Toggle based on current state
 });
 
-overlay.addEventListener("click", () => {
-  sidebar.classList.remove("open");
-  overlay.classList.remove("open");
+overlay.addEventListener("click", () => toggleSidebar(false));
+
+linkElements.forEach((element) => {
+  element.addEventListener("click", () => toggleSidebar(false));
+});
+
+// Smooth Scrolling
+
+const anchors = document.querySelectorAll('a[href^="#"]');
+anchors.forEach((anchor) => {
+  anchor.addEventListener("click", (event) => {
+    event.preventDefault();
+
+    const targetId = anchor.getAttribute("href");
+
+    const targetElement = document.querySelector(targetId);
+
+    if (targetElement) {
+      targetElement.scrollIntoView({
+        behavior: "smooth",
+      });
+    }
+  });
 });
